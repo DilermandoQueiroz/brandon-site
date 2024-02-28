babe3f623e09ac7ae7f91f46eca19a6e0144aa97
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2024-02-27 21:55:33 -0300

    Adiciona link para perfil de Instagram no componente de contato

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Contact: React.FC = () => {
     return (
-        <div className="w-full h-full flex-col justify-center items-center gap-2.5 inline-flex">
-            <div className="text-center text-5xl font-extrabold">Contact</div>
-            <div className="text-center text-xl">Brandon Frederico</div>
-        </div>
+          <div className="w-full h-full flex-col justify-center items-center gap-2.5 inline-flex">
+              <div className="text-center text-5xl font-extrabold">Contact</div>
+              <div className="text-center text-xl">Brandon Frederico</div>
+          </div>
     );
 };
 
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,14 +1,17 @@
 import Navigation from '@/components/Navigation'
 import Avatar2 from '@/components/Avatar2'
 import Contact from '@/components/Contact'
+import Link from 'next/link'
 
 
 export default function ContactPage() {
   return (
     <>
       <Navigation/>
-      <Avatar2/>
-      <Contact/>
+      <Link href="https://www.instagram.com/bfredericotattoos/">
+        <Avatar2/>
+        <Contact/>
+      </Link>
     </>
   )
 }
